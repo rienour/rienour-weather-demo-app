1b6bf1a0b123a8dd3022da12f24f90a37cadaae6
Rienour <R.D.Eichenour@gmail.com>
2025-07-27 07:55:42 -0500

    [API] - Install Firebase dependency and add baseline config
    The application will relay on Firebase for the Cloud operations of the
    platform. In order to support this functionality the Firebase SDK will
    be added to the application with the baseline configuration for the
    Firebase project.
    
    - Add FirebaseConfig to API configuration files
    - Install firebase SDK
    
    Signed-off-by: Rienour <R.D.Eichenour@gmail.com>

--- api/src/config.ts
+++ api/src/config.ts
@@ -4,6 +4,22 @@ dotenv.config();
 
 /**
  * This interface provides type safety for mapping the .env values loaded by
+ * dotenv related to the Firebase configuration.
+ *
+ * See {@link https://firebase.google.com/docs/web/learn-more?authuser=0#config-object}
+ * for more informaiton.
+ */
+interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+
+/**
+ * This interface provides type safety for mapping the .env values loaded by
  * dotenv into a config option
  */
 interface ApplicationConfig {
@@ -11,6 +27,10 @@ interface ApplicationConfig {
    * Port number the application will listen on.
    */
   port: number;
+  /**
+   * Configuraton for Firebase connector
+   */
+  firebase: FirebaseConfig;
 }
 
 /**
@@ -19,6 +39,21 @@ interface ApplicationConfig {
  */
 const config: ApplicationConfig = {
   port: Number(process.env.PORT),
+  firebase: {
+
+    apiKey: "AIzaSyCKCS38Rdba4_5_9jywtEZ52hJJoBef9b4",
+
+    authDomain: "rienour-weather-api-demo.firebaseapp.com",
+
+    projectId: "rienour-weather-api-demo",
+
+    storageBucket: "rienour-weather-api-demo.firebasestorage.app",
+
+    messagingSenderId: "352611853214",
+
+    appId: "1:352611853214:web:e1d2c09b550bafec1e6c8f"
+
+  }
 };
 
 export default config;
