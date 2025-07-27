fde091faa362873339781c7110f1bd060b97bf4b
Rienour <R.D.Eichenour@gmail.com>
2025-07-27 13:01:19 -0500

    [API] - Move all firebase pieces into Firebase file
    Signed-off-by: Rienour <R.D.Eichenour@gmail.com>

--- api/src/apps/firebase.ts
+++ api/src/apps/firebase.ts
@@ -1,7 +1,19 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import * as serviceSecret from "../config/secret_serviceAccountKey.json";
+import { getFirestore } from 'firebase-admin/firestore';
+
+/**
+ * This object provides the names of the collections available to Firestore to
+ * provide type safety when working with firestore.
+ */
+export const Collections = {
+  LocationWeathers: 'locationWeathers'
+} as const;
+export type CollectionName = typeof Collections[keyof typeof Collections];
 
 // FIXME: Remove casting
 const firebaseApp = initializeApp({ credential: cert(serviceSecret as ServiceAccount) });
+
+export const db = getFirestore();
 
 export default firebaseApp;
