45e8c26a437f1b85fd71c1c4cd422a023e3baebb
Rienour <R.D.Eichenour@gmail.com>
2025-07-28 19:00:38 -0500

    [API] - Install and enable cors() package
    Signed-off-by: Rienour <R.D.Eichenour@gmail.com>

--- api/src/apps/express.ts
+++ api/src/apps/express.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import locationRouter from '../routes/location';
 
 const expressApp = express();
 
 expressApp.use(express.json());
 
+expressApp.use(cors());
 expressApp.use('/location', locationRouter);
 
 export default expressApp;
