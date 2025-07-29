8e5298a0f644ee1e4bb9c4b9eaca96877644d8cd
Rienour <R.D.Eichenour@gmail.com>
2025-07-28 19:15:12 -0500

    [API] - Add express body parser middleware to express App
    Signed-off-by: Rienour <R.D.Eichenour@gmail.com>

--- api/src/apps/express.ts
+++ api/src/apps/express.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import locationRouter from '../routes/location';
+import bodyParser from 'body-parser';
 
 const expressApp = express();
 
 expressApp.use(express.json());
-
+expressApp.use(bodyParser.json());
 expressApp.use(cors());
 expressApp.use('/location', locationRouter);
 
